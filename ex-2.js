611a4d996183917e66c017746299c0c4d1f49297
Arisa Teandum <basarisa.1302@gmail.com>
2024-10-08 23:11:09 +0700

    feat: callbacks and higher order functions assignment

--- ex-2.js
+++ ex-2.js
@@ -1,23 +1,17 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
-  const roomPassCheck = operation(array);
-  if (roomPassCheck >= 5) {
-    return `ผ่านเกณฑ์ ✅`;
-  } else {
-    return `ไม่ผ่านเกณฑ์ ❌`;
-  }
-}
-
-function scoresCheck(array) {
-  let passStudent = 0;
-  for (let studentScore of array) {
-    if (studentScore > 70) {
-      passStudent += 1;
+  let count = 0;
+  for (let item of array) {
+    if (operation(item)) {
+      count++;
     }
   }
-  return passStudent;
+  return count >= 5 ? "ผ่านเกณฑ์ ✅" : "ไม่ผ่านเกณฑ์ ❌";
+}
+
+function isScoreMoreThan70(score) {
+  return score > 70;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -28,15 +22,17 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 let scoreRoom1Result = `นักเรียนห้องที่ 1 ${atLeastFive(
   studentScoresRoom1,
-  scoresCheck
+  isScoreMoreThan70
 )}`;
 let scoreRoom2Result = `นักเรียนห้องที่ 2 ${atLeastFive(
   studentScoresRoom2,
-  scoresCheck
+  isScoreMoreThan70
 )}`;
 let scoreRoom3Result = `นักเรียนห้องที่ 3 ${atLeastFive(
   studentScoresRoom3,
-  scoresCheck
+  isScoreMoreThan70
 )}`;
 
 console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
